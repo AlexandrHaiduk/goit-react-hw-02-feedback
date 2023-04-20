47df6947ea52400440c56de45f317a1aefb635f2
AlexandrHaiduk <alexanderhaiduk@gmail.com>
2023-04-20 13:41:30 +0300

    start

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,10 @@
+export const Feedback = () => {
+  return (
+    <>
+      <h1>Please leave feedback</h1>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+    </>
+  );
+};
