c8332d41526b9f7a0d28e7901062183c42fe6194
AlexandrHaiduk <alexanderhaiduk@gmail.com>
2023-04-22 15:33:01 +0300

    some changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,24 @@
+import React, { Component } from 'react';
 import { Feedback } from './Feedback';
 import { Stats } from './Stats';
 
-export const App = () => {
-  return (
-    <>
-      <Feedback />
-      <Stats />
-    </>
-  );
-};
+export class App extends Component {
+  state = {
+    bad: 0,
+    neutral: 0,
+    good: 0,
+  };
+
+  addFeedback = event => {
+    console.log(event.target);
+  };
+
+  render() {
+    return (
+      <>
+        <Feedback onClick={this.addFeedback} />
+        <Stats />
+      </>
+    );
+  }
+}
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,10 +1,23 @@
-export const Feedback = () => {
+export const Feedback = ({ addFeedback }) => {
   return (
     <>
       <h1>Please leave feedback</h1>
-      <button>Good</button>
-      <button>Neutral</button>
-      <button>Bad</button>
+      <button
+        type="button"
+        name="good"
+        onClick={e => {
+          console.log(e);
+          console.log(e.currentTarget);
+        }}
+      >
+        Good
+      </button>
+      <button type="button" name="neutral" onClick={addFeedback}>
+        Neutral
+      </button>
+      <button type="button" name="bad" onClick={addFeedback}>
+        Bad
+      </button>
     </>
   );
 };
